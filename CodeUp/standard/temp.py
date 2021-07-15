860b476aa2a8c37371c3d588e74912076231201e
ekfaktldk11 <ekfaktldk11@naver.com>
2021-07-15 17:48:12 +0900

    Greedy & Implementation practice

--- CodeUp/standard/temp.py
+++ CodeUp/standard/temp.py
@@ -1,29 +1,12 @@
-# import sys
-# maze = []
-# for i in range(10):
-#     maze.append(list(map(int, sys.stdin.readline().split)))
+n, k = map(int, input().split())
+count = 0
 
-maze = []
-for i in range(10):
-    temp = input().split()
-    for j in range(10):
-        temp[i] = int(temp[i])
-    maze.append(temp)
-
-x, y = 1, 1
-while (1):
-    maze[x][y] = 9
-    if (maze[x][y + 1] == 1):
-        if (maze[x + 1][y]):
-            break
-        else:
-            x = x + 1
-    elif (maze[x][y + 1] == 2):
-        break
+while True:
+    if(n == 1): break
+    elif(n % k == 0):
+        n = n / k
+        count += 1
     else:
-        y = y + 1
-
-for i in range(10):
-    for j in range(10):
-        print(maze[i][j], end=" ")
-    print()
+        n = n - 1
+        count += 1
+print(count)
