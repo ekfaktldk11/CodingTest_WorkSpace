528f8708087da5b2333250d4bd79f3b0c9943d97
ekfaktldk11 <ekfaktldk11@naver.com>
2021-07-20 21:38:40 +0900

    bfs practice

--- CodeUp/standard/temp.py
+++ CodeUp/standard/temp.py
@@ -1,66 +1,41 @@
-# n, m = map(int, input().split())
-#
-# ary = []
-#
-# for i in range(n):
-#     ary.append(list(map(int, input().split())))
-#
-# print(ary)
-# def dfs(x, y):
-#     if x < 0 or x > n or y < 0 or y > m: #범위 초과
-#         return False
-#     if ary[x][y] == 0: # 처음 채우는 음료수 군집
-#         ary[x][y] == 1
-#         dfs(x - 1, y) #상
-#         dfs(x + 1, y) #하
-#         dfs(x, y - 1) #좌
-#         dfs(x, y + 1) #우
-#         return True
-#     return False
-#
-# count = 0
-# for i in range(n):
-#     for j in range(m):
-#         if dfs(i, j) == True: # 새로운 군집 형성시 count ++
-#             count += 1
-#
-# print(count)
+from collections import deque
 
 # N, M을 공백으로 구분하여 입력받기
 n, m = map(int, input().split())
-
 # 2차원 리스트의 맵 정보 입력받기
 graph = []
 for i in range(n):
-    # 여기서 중요한 것이 graph.append(list(map(int, input().split())))로 실행 시 [[000111011], [110110010], ...] 이런식으로 할당됨
-    # 공백없이 입력될 때는 graph.append(list(map(int, input())))로 실행 해야 [[0, 1, 0 ...]] 이런식으로 할당됨
     graph.append(list(map(int, input())))
 
-print(graph)
+# 이동할 네 방향 정의(상, 하, 좌, 우)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-# DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
-def dfs(x, y):
-    #global graph
-    # 주어진 범위를 벗어나는 경우에는 즉시 종료
-    if x <= -1 or x >= n or y <= -1 or y >= m:
-        return False
-    # 현재 노드를 아직 방문하지 않았다면 (음료수를 채우지 않았다면)
-    if graph[x][y] == 0:
-        # 해당 노드를 방문 처리 (빈칸에 음료수 채우기)
-        graph[x][y] = 1
-        dfs(x - 1, y)
-        dfs(x, y - 1)
-        dfs(x + 1, y)
-        dfs(x, y + 1)
-        return True
-    return False
+# BFS 소스코드 구현
+def bfs(x, y):
+    # 큐 구현을 위해 deque 라이브러리 사용
+    queue = deque()
+    queue.append((x, y))
+    # 큐가 빌 때까지 반복
+    while queue:
+        x, y = queue.popleft()
+        # 현재 위치에서 네 방향 확인
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            # 미로 찾기 공간을 벗어난 경우 무시
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            # 벽인 경우 무시
+            if graph[nx][ny] == 0:
+                continue
+            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
 
-# 모든 노드(위치)에 대하여 음료수 채우기
-result = 0
-for i in range(n):
-    for j in range(m):
-        # 현재 위치에서 DFS 수행
-        if dfs(i, j) == True:
-            result += 1
+    return graph[n - 1][m - 1]
 
-print(result) # 정답 출력
+# BFS를 수행한 결과 출력
+print(bfs(0, 0))
+
+# 왜 처음 방문하는 경우에 최단 거리를 기록하면서 큐에 넣고 선입선출 하는것을 큐가 빌때까지 반복하면 미로의 최단거리가 나오는것일까?
