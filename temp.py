9a6b282dbc07b5564ea815a866ab8f98e5676263
ekfaktldk11 <ekfaktldk11@naver.com>
2021-07-26 17:40:11 +0900

    binary search practice_2

--- temp.py
+++ temp.py
@@ -1,20 +1,31 @@
-import sys
+# 떡의 개수(N)와 요청한 떡의 길이(M)을 입력받기
+n, m = list(map(int, input().split()))
 
-# 부품 개수
-n = int(input())
-n_ary = set(map(int, sys.stdin.readline().split()))
-# n_ary = set(map(int, sys.stdin.readline().split()))
+# 각 떡의 개별 높이 정보를 입력받기
+array = list(map(int, input().split()))
 
-print(n_ary)
-# n_ary.sort()
+# 이진 탐색을 위하 시작점과 끝점 설정
+start = 0
+end = max(array)
 
-# 주문
-m = int(input())
-m_ary = list(map(int, sys.stdin.readline().split()))
-
-
-for i in range(m):
-    if m_ary[i] in n_ary:
-        print('yes', end=' ')
+# 이진 탐색 수행(반복적)
+result = 0
+while(start <= end):
+    total = 0
+    mid = (start + end) // 2
+    for x in array:
+        # 잘랐을 때 떡의 양 계산
+        if x > mid:
+            total += (x - mid)
+    # 떡의 양이 부족한 경우 (search left part)
+    if total == m:
+        print(mid)
+    # 떡의 양이 충분한 경우 (search right part)
+    elif total < m:
+        end = mid - 1
     else:
-        print('no', end=' ')
+        result = mid # !!! 최대한 덜 잘랐을 때가 정답이므로, result 를 계속 기록
+        start = mid + 1
+
+# 정답 출력
+print(result)
