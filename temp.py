7bf6ca34fd36c014b6e79b95bc92a17c6179c5d6
ekfaktldk11 <ekfaktldk11@naver.com>
2021-07-27 17:35:33 +0900

    trivial

--- temp.py
+++ temp.py
@@ -1,31 +1,30 @@
+import sys
+# 채택된 식량 창고 check
+d = [0] * 100
+
+# 식량 창고 수
 n = int(input())
 
-ary = [0] * (n + 1)
+# 창고 마다의 식량 수
+n_ary = list(map(int, sys.stdin.readline().split()))
+n_ary.append(0)
+n_ary.append(0)
+n_ary.append(0)
+n_ary.append(0)
+n_ary.append(0)
+n_ary.append(0)
 
-print(ary)
 
-oper = [5, 3, 2]
+print(n_ary)
 
-def f(x):
-    if x == 1:
-        return 0
-    if x == 2:
-        return 1
-    if x == 3:
-        return 1
-    if x == 4:
-        return 2
-    if x == 5:
-        return 1
+for i in range(0, n + 2, 2):
+    r1 = n_ary[i] + max(n_ary[i + 2], n_ary[i + 3])
+    r2 = n_ary[i + 1] + max(n_ary[i + 3], n_ary[i + 4])
+    if (r1 >= r2): d[i] = 1
+    else: d[i + 1] = 1
 
-    if ary[x] != 0:
-        return ary[x]
-
-    for i in range(3):
-        if x % oper[i] == 0:
-            ary[x] = f(x // oper[i]) + 1
-            return ary[x]
-    ary[x] = f(x - 1) + 1
-    return ary[x]
-
-print(f(n))
+sum = 0
+for i in range(n):
+    if d[i] == 1: sum += n_ary[i]
+print(d)
+print(sum)
