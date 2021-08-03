19f47c6920014b75946a4d4f2ade4afa2bf10137
ekfaktldk11 <ekfaktldk11@naver.com>
2021-08-03 18:49:49 +0900

    trivial

--- temp.py
+++ temp.py
@@ -1,23 +1,29 @@
-# 정수 N을 입력받기
 n, m = map(int, input().split())
 
-n_ary = []
-for i in range(n):
-    n_ary.append(int(input()))
+INF = int(1e9)
 
-n_ary.sort()
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트 생성
+graph = [[INF] * (n + 1) for i in range(n + 1)]
 
-d = [10001] * (m + 1)
-d[0] = 0
+# (빼먹은 부분) 자기 자신에서 자기 자신으로 가는 비용 0으로 초기화
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
 
-# i - k 원을 만들 수 있는 경우 & 그럴 수 없는 경우를 나눠서 탑 다운
 
-for i in range(n):
-    for j in range(n_ary[i], m + 1):
-        if d[j - n_ary[i]] != 10001:
-            d[j] = min(d[j], d[j - n_ary[i]] + 1)
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1 # (빼먹은 부분)
 
-if(d[m] == 10001):
-    print(-1)
-else:
-    print(d[m])
+x, k = map(int, input().split())
+
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        for c in range(1, n + 1):
+            graph[b][c] = min(graph[b][c], graph[b][a] + graph[a][c])
+
+ans = graph[1][k] + graph[k][x]
+if ans >= INF: print(-1)
+else: print(ans)
