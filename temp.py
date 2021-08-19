02b3536e0a8e658166017d38611acff82821a047
ekfaktldk11 <ekfaktldk11@naver.com>
2021-08-19 16:20:13 +0900

    #30 solved but time_over

--- temp.py
+++ temp.py
@@ -1,20 +1,49 @@
-n = int(input())
+words = []
+queries = []
 
-ary = list(map(int, input().split()))
+for _ in range(6):
+    temp = input()
+    words.append(temp)
 
-
-def binary_search(start, end):
-
-    if start > end: return -1
-
-    mid = (start+end) // 2
-
-    if mid == int(ary[mid]):
-        return mid
-    elif mid > int(ary[mid]):
-        return binary_search(mid + 1, end)
-    else:
-        return binary_search(start, mid - 1)
+for _ in range(5):
+    temp = input()
+    queries.append(temp)
 
 
-print(binary_search(0, n - 1))
+def solution(words, queries):
+    listify = []
+    answer = []
+
+    for _ in range(100001):
+        listify.append([])
+
+    for word in words:
+        listify[len(word)].append(word)
+
+    for query in queries:
+
+        count = 0
+        tail = len(query)
+        head = 0
+
+        q_len = len(query)
+        if query.find('?') == 0:
+            head = query.rfind('?') + 1
+        else:
+            tail = query.find('?')
+
+        q_str = query[head:tail]
+
+        # q_str, q_len
+        if head == 0:
+            for word in listify[q_len]:
+                if word.find(q_str) == 0: count += 1
+        else:
+            for word in listify[q_len]:
+                if word.rfind(q_str) == (q_len - len(q_str)): count += 1
+        answer.append(count)
+
+    return answer
+
+
+print(solution(words, queries))
