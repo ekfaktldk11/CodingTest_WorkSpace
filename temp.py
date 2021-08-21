adaa7ed1cf363bd3ed9fbd5eb75d85956d2f8765
ekfaktldk11 <ekfaktldk11@naver.com>
2021-08-21 22:27:21 +0900

    #32 solved

--- temp.py
+++ temp.py
@@ -1,43 +1,19 @@
- # 565 pg 확인
-t = int(input())
+n = int(input())
 
-# 오른쪽 위, 오른쪽, 오른쪽 아래
-dx = [-1, 0, 1]
-
-
-def backward_value(updated_graph, x, y, n, m):
-    if x >= 1 and x < (n + 1) and y >= 1 and y < (m + 1):
-        return updated_graph[x][y]
-    else:
-        return 0
-
-def solution():
-    n, m = map(int, input().split())
-
-    ary = [[0] * (m + 1) for _ in range(n + 1)]
-
-    d = [[0] * (m + 1) for _ in range(n + 1)]
-
+dp = []
+for _ in range(n):
     temp = list(map(int, input().split()))
-    for i in range(1, n + 1):
-        for j in range(1, m + 1):
-            ary[i][j] = temp.pop(0)
+    dp.append(temp)
 
 
-    for j in range(1, m + 1):
-        for i in range(1, n + 1):
-            for k in range(3):
-               d[i][j] = max(d[i][j], ary[i][j] + backward_value(d, i + dx[k], j - 1, n, m))
+for i in range(1, n):
+    m = len(dp[i])
+    for k in range(m):
+        if k == 0:
+            dp[i][k] = dp[i][k] + dp[i - 1][k]
+        elif k == (m - 1):
+            dp[i][k] = dp[i][k] + dp[i - 1][k - 1]
+        else:
+            dp[i][k] = dp[i][k] + max(dp[i - 1][k - 1], dp[i - 1][k])
 
-    max_val = 0
-    # for i in range(1, n + 1):
-    #     for j in range(1, m + 1):
-    #         max_val = max(max_val, d[i][j])
-    for i in range(n):
-        max_val = max(max_val, d[i][m]) # 짜피 각 라인에서 맨 끝의 값들이 제일 큰 값을 가질수밖에 없음
-
-    print(d)
-    print(max_val)
-
-for g in range(t):
-    solution()
+print(max(dp[n-1]))
