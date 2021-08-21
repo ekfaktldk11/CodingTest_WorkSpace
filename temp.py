2b53e77860023b11544e5cd166266a2a4232da71
ekfaktldk11 <ekfaktldk11@naver.com>
2021-08-21 21:51:47 +0900

    #31 solved

--- temp.py
+++ temp.py
@@ -1,49 +1,43 @@
-words = []
-queries = []
+ # 565 pg 확인
+t = int(input())
 
-for _ in range(6):
-    temp = input()
-    words.append(temp)
-
-for _ in range(5):
-    temp = input()
-    queries.append(temp)
+# 오른쪽 위, 오른쪽, 오른쪽 아래
+dx = [-1, 0, 1]
 
 
-def solution(words, queries):
-    listify = []
-    answer = []
+def backward_value(updated_graph, x, y, n, m):
+    if x >= 1 and x < (n + 1) and y >= 1 and y < (m + 1):
+        return updated_graph[x][y]
+    else:
+        return 0
 
-    for _ in range(100001):
-        listify.append([])
+def solution():
+    n, m = map(int, input().split())
 
-    for word in words:
-        listify[len(word)].append(word)
+    ary = [[0] * (m + 1) for _ in range(n + 1)]
 
-    for query in queries:
+    d = [[0] * (m + 1) for _ in range(n + 1)]
 
-        count = 0
-        tail = len(query)
-        head = 0
-
-        q_len = len(query)
-        if query.find('?') == 0:
-            head = query.rfind('?') + 1
-        else:
-            tail = query.find('?')
-
-        q_str = query[head:tail]
-
-        # q_str, q_len
-        if head == 0:
-            for word in listify[q_len]:
-                if word.find(q_str) == 0: count += 1
-        else:
-            for word in listify[q_len]:
-                if word.rfind(q_str) == (q_len - len(q_str)): count += 1
-        answer.append(count)
-
-    return answer
+    temp = list(map(int, input().split()))
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            ary[i][j] = temp.pop(0)
 
 
-print(solution(words, queries))
+    for j in range(1, m + 1):
+        for i in range(1, n + 1):
+            for k in range(3):
+               d[i][j] = max(d[i][j], ary[i][j] + backward_value(d, i + dx[k], j - 1, n, m))
+
+    max_val = 0
+    # for i in range(1, n + 1):
+    #     for j in range(1, m + 1):
+    #         max_val = max(max_val, d[i][j])
+    for i in range(n):
+        max_val = max(max_val, d[i][m]) # 짜피 각 라인에서 맨 끝의 값들이 제일 큰 값을 가질수밖에 없음
+
+    print(d)
+    print(max_val)
+
+for g in range(t):
+    solution()
