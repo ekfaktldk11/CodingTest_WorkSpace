2f2fcbe7a747cc32a687fd81f8436e23e525bfa9
ekfaktldk11 <ekfaktldk11@naver.com>
2021-08-23 17:09:53 +0900

    #40 solved

--- temp.py
+++ temp.py
@@ -1,49 +1,39 @@
 import heapq
 
-t = int(input())
+n, m = map(int, input().split())
+INF = int(1e9)
+graph = [[] for _ in range(n + 1)]
 
-# 상 하 좌 우
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append((1, b))
+    graph[b].append((1, a))
 
-def dij():
-    n = int(input())
+distance = [INF] * (n + 1)
 
-    INF = int(1e9)
-
-    graph = []
-
-    distance = [[INF] * n for _ in range(n)]
-
-    def valid(x, y):
-        if x >= 0 and y >= 0 and x < n and y < n:
-            return True
-        else:
-            return False
-
-    for i in range(n):
-        temp = list(map(int, input().split()))
-        graph.append(temp)
-
+def dij(start):
     q = []
-
-    heapq.heappush(q, (graph[0][0], 0, 0))
-
-    distance[0][0] = graph[0][0]
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
 
     while q:
-        dist, x, y = heapq.heappop(q)
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist: continue
 
-        if distance[x][y] < dist: continue
+        for i in graph[now]:
+            cost = dist + i[0]
+            if cost < distance[i[1]]:
+                distance[i[1]] = cost
+                heapq.heappush(q, (cost, i[1]))
 
-        for d in range(4):
-            nx = x + dx[d]
-            ny = y + dy[d]
-            if not valid(nx, ny): continue
-            cost = dist + graph[nx][ny]
-            if distance[nx][ny] > cost:
-                distance[nx][ny] = cost
-                heapq.heappush(q, (cost, nx, ny)) # dist 값이 들어가는게 아니라 cost값이 들어가야함 !!
-    print(distance[n - 1][n - 1])
+dij(1)
+max_val = max(distance[2:])
+max_list = []
+for i in range(2, n + 1):
+    if distance[i] == max_val:
+        max_list.append(i)
 
-for _ in range(t): dij()
+max_list.sort()
+print(max_list[0], end=' ')
+print(max_val, end=' ')
+print(len(max_list))
